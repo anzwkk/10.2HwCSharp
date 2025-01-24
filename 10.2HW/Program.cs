9eb26c7614b5a78e2912957ba8ee013002df6d30
anzwkk <anam89737@gmail.com>
2025-01-25 02:49:56 +0300

    Added a structure for creating a text report

--- 10.2HW/Program.cs
+++ 10.2HW/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+StringBuilder report = new StringBuilder();
+
+Console.WriteLine("Введіть заголовок звіту:");
+string title = Console.ReadLine();
+
+Console.WriteLine("Введіть дату звіту: ");
+string dateInput = Console.ReadLine();
+
+while (true)
+{
+    Console.WriteLine("Введіть події: ");
+    string eventInput = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(eventInput));
+    {
+        break;
+    }
+}
